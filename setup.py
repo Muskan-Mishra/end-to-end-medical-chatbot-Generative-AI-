0d66f1d52a2897809c651c62bc11dc2d76704557
Muskan Kumari <98957217+Muskan-Mishra@users.noreply.github.com>
2026-04-10 12:11:15 +0530

    Update author information in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="Healthcare-chatbot",
     version="0.1",
-    author= 'Monalisha Bangal',
-    author_email= 'monalishabangal2021@gmail.com',
+    author= 'Muskan Kumari',
+    author_email= 'muskanmishra1403@gmail.com',
     packages=find_packages(),
     install_requires=[],  # add any ML dependencies here
 )
